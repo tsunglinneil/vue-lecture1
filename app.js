0c084c03a42d003ab5524851d28b26a7e9de551f
tsunglinneil <magic3657@gmail.com>
2020-02-19 01:27:54 +0800

    init

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    status: false,
+    days: ['1', '2', '3'],
+    movie: {
+      title: 'Interstellar',
+      year: '2014',
+      star: "5"
+    }
+  }
 });
