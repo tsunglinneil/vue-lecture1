049b9f4628353ab4f16ed43db2ea046e11b4bde5
tsunglinneil <magic3657@gmail.com>
2020-02-18 23:52:28 +0800

    init

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    greet: 'Hello World',
+    name: 'Louis'
+  },
+  methods: {
+    //ES5
+    greeting1: function() {
+      return 'Greeting 1: ' + this.greet + ' ! ' + this.name + ' ~';
+    },
+    //ES6
+    greeting2() {
+      return `Greeting 2: ${this.greet} ! ${this.name} ~`;
+    }
+  }
 });
