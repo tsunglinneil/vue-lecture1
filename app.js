6051faea248d24d65c39180e58997d1399fbd302
tsunglinneil <magic3657@gmail.com>
2020-02-19 01:02:30 +0800

    init

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    firstCount: 1,
+    secondCount: 1
+  },
+  computed: {
+    descComputed() {
+      console.log("descComputed");
+      return this.firstCount > 3 ? 'firstCount > 3' : 'firstCount < 3';
+    },
+    showAllCount() {
+      console.log("showAllCount");
+      return `firstCount is ${this.firstCount} | secondCount is ${this.secondCount}`;
+      // return this.firstCount > 3 ? 'firstCount > 3' : 'firstCount < 3';
+    }
+  },
+  watch: {
+    firstCount(newFirstCount, oldFirstCount) {
+      console.log("watch firstCount => ", newFirstCount, oldFirstCount);
+    },
+    secondCount(newSecondCount, oldSecondCount) {
+      console.log("watch secondCount => ", newSecondCount, oldSecondCount);
+    }
+  },
+  methods: {
+    increase() {
+      console.log("increase");
+      this.firstCount++;
+    },
+    decrease() {
+      console.log("decrease");
+      this.firstCount--;
+    },
+    descMethod() {
+      console.log("descMethod");
+      return this.firstCount > 3 ? 'firstCount > 3' : 'firstCount < 3';
+    },
+    increaseSecond() {
+      console.log("increaseSecond");
+      this.secondCount++;
+    }
+  }
 });
